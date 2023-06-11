6e52ce328554e842299fbf80a98f2cb51c2e75e7
theRobert174 <rodrigomtzmacias@hotmail.com>
2023-06-11 11:48:42 -0600

    heroImage pipe

--- src/app/heroes/heroes.module.ts
+++ src/app/heroes/heroes.module.ts
@@ -2,13 +2,17 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { MaterialModule } from '../material/material.module';
-
 import { HeroesRoutingModule } from './heroes-routing.module';
+
+import { HeroImagePipe } from './pipes/hero-image.pipe';
+
 import { HeroPageComponent } from './pages/hero-page/hero-page.component';
 import { LayoutPageComponent } from './pages/layout-page/layout-page.component';
 import { ListPageComponent } from './pages/list-page/list-page.component';
 import { NewPageComponent } from './pages/new-page/new-page.component';
 import { SearchPageComponent } from './pages/search-page/search-page.component';
+
+import { CardComponent } from './components/card/card.component';
 
 
 @NgModule({
@@ -17,7 +21,11 @@ import { SearchPageComponent } from './pages/search-page/search-page.component';
     LayoutPageComponent,
     ListPageComponent,
     NewPageComponent,
-    SearchPageComponent
+    SearchPageComponent,
+
+    CardComponent,
+
+    HeroImagePipe
   ],
   imports: [
     CommonModule,
