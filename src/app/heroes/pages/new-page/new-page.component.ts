c306b42020619bfff50afd88b40b4faf183e7fc1
theRobert174 <rodrigomtzmacias@hotmail.com>
2023-06-12 21:23:28 -0600

    reactive Form bind to template

--- src/app/heroes/pages/new-page/new-page.component.ts
+++ src/app/heroes/pages/new-page/new-page.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { FormControl, FormGroup } from '@angular/forms';
+import { Publisher } from '../../interfaces/hero.interface';
 
 @Component({
   selector: 'app-new-page',
@@ -12,5 +14,24 @@ export class NewPageComponent {
     {id: 'DC Comics', desc: 'DC - Comics'},
     {id: 'Marvel Comics', desc: 'Marvel - Comics'}
   ]
+
+  public heroForm = new FormGroup({
+    id :                new FormControl<string>(''),
+    superhero :         new FormControl<string>('', { nonNullable: true }),
+    publisher :         new FormControl<Publisher>(Publisher.DCComics),
+    alter_ego :         new FormControl<string>(''),
+    first_appearance :  new FormControl<string>(''),
+    characters :        new FormControl<string>(''),
+    alt_img :           new FormControl<string>(''),
+  });
+
+
+
+  onSubmit() : void {
+    console.log({
+      formIsValid: this.heroForm.valid,
+      value: this.heroForm.value,
+    })
+  }
 
 }
